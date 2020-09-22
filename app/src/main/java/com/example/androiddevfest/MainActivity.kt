e865e81406d9d65fcfef9f30f6ddb7f82212eb29
Asilbek Tilavov <thebukharianxo@mail.ru>
2020-09-22 17:08:40 +0500

    Recycler view added

--- app/src/main/java/com/example/androiddevfest/MainActivity.kt
+++ app/src/main/java/com/example/androiddevfest/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.androiddevfest
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_main.*
+import www.sanju.zoomrecyclerlayout.ZoomRecyclerLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +25,55 @@ class MainActivity : AppCompatActivity() {
         animationDrawable.setEnterFadeDuration(4000)
         animationDrawable.setExitFadeDuration(4000)
         animationDrawable.start()
+
+        //implement links
+                instaBtn.setOnClickListener{
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.instagram.com/gdgtashkent/?hl=ru")
+            startActivity(openURL)
+        }
+                faceBtn.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.facebook.com/gdgtashkent/")
+            startActivity(openURL)
+        }
+                youtubeBtn.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.youtube.com/channel/UCEPvlcWSQJ-lsv-7v20mFBQ")
+            startActivity(openURL)
+        }
+                twitterBtn.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://twitter.com/gdgtashkent")
+            startActivity(openURL)
+        }
+
+
+        val adapter=GroupAdapter<GroupieViewHolder>()
+        adapter.add(FirstPageItems())
+        adapter.add(FirstPageItems())
+        adapter.add(FirstPageItems())
+        myRecycler.adapter=adapter
+
+        val linearLayoutManager = ZoomRecyclerLayout(this)
+        val snapHelper = LinearSnapHelper()
+        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        linearLayoutManager.reverseLayout = true
+        linearLayoutManager.stackFromEnd = true
+        myRecycler.layoutManager = linearLayoutManager
+        snapHelper.attachToRecyclerView(myRecycler)
+        myRecycler.isNestedScrollingEnabled = false
+
     }
+}
+
+class FirstPageItems: Item<GroupieViewHolder>(){
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.first_page_item
+    }
+
 }
