cd0dd9c1c04814cf1c67869656a44f77084195b0
Asilbek Tilavov <thebukharianxo@mail.ru>
2020-09-28 14:03:04 +0500

    Speakers to fetch staff

--- app/src/main/java/com/example/androiddevfest/Options/Speakers.kt
+++ app/src/main/java/com/example/androiddevfest/Options/Speakers.kt
@@ -1,12 +1,88 @@
 package com.example.androiddevfest.Options
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androiddevfest.Adapters.AgendaAdapter
+import com.example.androiddevfest.MainActivity
 import com.example.androiddevfest.R
+import com.example.androiddevfest.SaveData
+import kotlinx.android.synthetic.main.activity_agenda.*
+import kotlinx.android.synthetic.main.activity_agenda.actBarAgenda
+import kotlinx.android.synthetic.main.activity_agenda.arrowAgenda
+import kotlinx.android.synthetic.main.activity_agenda.lightBtnAgenda
+import kotlinx.android.synthetic.main.activity_speakers.*
 
 class Speakers : AppCompatActivity() {
+    lateinit var saveData: SaveData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_speakers)
+
+
+
+//==================================================================================================
+
+        themeCheck()
+        arrowAgenda.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+        lightBtnAgenda.setOnClickListener {
+            if (!saveData.loadDarkModeState()) {
+                saveData.setDarkNodeState(true)
+                darkSet()
+                finish()
+                overridePendingTransition(0, 0)
+                startActivity(intent)
+                overridePendingTransition(0, 0)
+
+
+            }else{
+                saveData.setDarkNodeState(false)
+                darkOff()
+                finish();
+                overridePendingTransition(0, 0);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            }
+        }
+
+
+
+
     }
+
+    fun themeCheck(){
+        saveData = SaveData(this)
+        if (saveData.loadDarkModeState()){
+            darkSet()
+        }else{
+            darkOff()
+        }
+    }
+    private fun darkSet(){
+        setTheme(R.style.DarkTheme)
+        lightBtnAgenda.setImageResource(R.drawable.darklight)
+        tabLayout.setBackgroundResource(R.drawable.gradient_dark)
+        actBarAgenda.setBackgroundResource(R.drawable.act_darkoption)
+        tabLayout.getTabAt(0)!!.setIcon(R.drawable.cloud_black)
+        tabLayout.getTabAt(1)!!.setIcon(R.drawable.phone_black)
+        tabLayout.getTabAt(2)!!.setIcon(R.drawable.web_black)
+
+
+    }
+    private fun darkOff(){
+        setTheme(R.style.AppTheme)
+        lightBtnAgenda.setImageResource(R.drawable.light)
+        tabLayout.setBackgroundResource(R.drawable.actlight)
+        actBarAgenda.setBackgroundResource(R.drawable.actlight)
+        tabLayout.getTabAt(0)!!.setIcon(R.drawable.baseline_cloud_queue_black_18dp)
+        tabLayout.getTabAt(1)!!.setIcon(R.drawable.baseline_stay_primary_portrait_black_36dp)
+        tabLayout.getTabAt(2)!!.setIcon(R.drawable.baseline_public_black_36dp)
+    }
+
 }
